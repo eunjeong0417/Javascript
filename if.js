3225dc300190a11076a2024743dc7ab49e77629b
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-05-13 00:01:39 +0900

    Create if.js

---
+++ if.js
@@ -0,0 +1,72 @@
+if(10 > 5){
+    console.log('hi')
+} else {
+    console.log('ho')
+}
+
+
+
+if(10 === 5){
+    console.log('hi')
+} else {
+    console.log('ho')
+}
+
+
+if("10" === 5){
+    console.log('hi')
+} else {
+    console.log('ho')
+}
+
+
+
+if("10" === 10){
+    console.log('hi')
+} else if("10"==="10"){
+    console.log("same")
+} else{
+    console.log("flase")
+}
+
+
+
+
+if(20 > 5 && "e"==="e"){
+    console.log('yes');
+} else {
+    console.log("no");
+}
+
+
+
+if(20 > 5 || "e"==="r"){
+    console.log('yes');
+} else {
+    console.log("no");
+}
+
+
+
+if(age >= 18 && age <=21){
+    console.log("hello")
+} else if (age > 21){
+    console.log("hi")
+} else{
+    console.log("too young")
+}
+
+
+
+const score = 75;
+if (score >=90){
+    console.log('A');
+} else if(score >=80){
+    console.log('B');
+} else if(score >=70){
+    console.log('C');
+} else if(score >=60) {
+    console.log('D');
+}
+
+
