e338491aa79d87d1095dac08e7c50ce075ecbdb2
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-05-12 23:14:25 +0900

    Create while.js

---
+++ while.js
@@ -0,0 +1,69 @@
+let a = 0
+while ( a < 5) {
+    console.log('hello')
+    a = a + 1
+}
+
+
+
+let a = 0
+while ( a < 5) {
+    console.log(a)
+    a = a + 1
+}
+
+
+
+
+let a = 0
+while ( a < 10) {
+    alert(a)
+    a++;
+}
+
+
+
+let a = 10;
+while (a) {
+    alert(a)
+    a--;
+}
+
+
+let a = 10;
+while(a) alert(a--);
+
+
+let i = 0;
+while(i < 100){
+    console.log('Hello, World');
+    i++;
+}
+
+let i = 0;
+while(i <= 99){
+    console.log('Hello, World');
+    i++;
+}
+
+
+let i = 1;
+while(i <= 100){
+    console.log('Hello, World');
+    i++;
+}
+
+let i = 1;
+while(i <= 100){
+    console.log('Hello');
+    i+=1;
+
+}
+
+let i = 1;
+while(i <= 50){
+    console.log('Hello');
+    i+=1;
+    
+}
+
