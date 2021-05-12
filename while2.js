c84b0094ed4b8556e6872e65b978a6500324981e
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-05-13 00:02:35 +0900

    Create while2.js

---
+++ while2.js
@@ -0,0 +1,49 @@
+for (let i = 0; i < 100; i++) {
+    console.log('Hello World');
+} 
+
+
+for(i = 0; i < 100; i += 1){
+    console.log(i+1);
+}
+
+
+for(i = 0; i < 100; i = i + 1){
+    console.log(i+1);
+}
+
+
+for(i = 0; i < 20; i = i + 2){
+    console.log(i+1);
+}
+
+for(i = 0; i < 30; i = i + 3){
+    console.log(i+1);
+}
+
+
+let i = 0;
+while (i < 100){
+    console.log('Hello World')
+    i++;
+}
+
+let i = 5;
+while (i < 100){
+    console.log('Hello World')
+    i++;
+}
+
+let i = 0;
+while (i < 100){
+    console.log(i)
+    i++;
+}
+
+let i = 0;
+while (i < 100){
+    console.log(i + 1)
+    i++;
+}
+
+
