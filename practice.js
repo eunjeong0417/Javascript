81a548457c69b4b55b755174bd20e80cf05f52c3
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-05-13 23:03:48 +0900

    Update practice.js

--- practice.js
+++ practice.js
@@ -78,3 +78,53 @@
     </script>
 </body>
 </html>
+
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{font-size: 3rem;}
+        h1{
+            background-color: yellowgreen;
+        }
+        .ilbuni{
+            display:inline-block;
+            list-style:none;
+        }
+        .link-google {
+            color:red;
+        }
+        .special {
+            background-color: pink;
+        }
+    </style>
+</head>
+<body>
+    <h1 id="main-title">
+        <span>hello</span>
+        DOM (Document Object Model)
+    </h1>
+    <ul>
+        <li class="ilbuni">
+            <img src="ilbuni-1.png">
+            <a class="link-google" href="https://google.com">Google</a>
+        </li>
+        <li class="ilbuni"><img src="ilbuni-2.png"></li>
+        <li class="ilbuni"><img src="ilbuni-3.png"></li>
+    </ul>
+
+    <script>
+        var link = document.querySelector('.ilbuni a');
+        //link.className = 'special';
+        link.classList.add('special');
+        setTimeout (function (){
+            link.classList.remove('link-google');
+        }, 5000);
+    </script> 
+</body>
+</html>
