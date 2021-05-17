7dbb1f173b2b39e3faaab1fb7c912685c336a8ed
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-05-17 23:20:35 +0900

    Create symbol.js

---
+++ symbol.js
@@ -0,0 +1,9 @@
+let testSymbol1 = Symbol(1);
+let testSymbol2 = Symbol(1);
+
+console.log(typeof testSymbol1, testSymbol1);
+console.log(typeof testSymbol2, testSymbol2);
+
+console.log(testSymbol1 == testSymbol2);
+console.log(testSymbol1 === testSymbol2);
+
