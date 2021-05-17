4e8f07d951c11bbc5cff196cf49c0adc896099d9
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-05-17 13:21:49 +0900

    Create eventListener.js

---
+++ eventListener.js
@@ -0,0 +1,25 @@
+
+1)
+
+document.querySelector('input').addEventListener('input', function(){
+    console.log('typing');
+});
+
+document.querySelector('button').addEventListener('click', function(){
+    console.log('click');
+});
+
+
+
+2)
+
+
+document.querySelector('input').addEventListener('input', function(){
+    console.log('typing');
+});
+
+const onClick = function(){
+    console.log('click');
+}
+
+document.querySelector('button').addEventListener('click', onClick);
