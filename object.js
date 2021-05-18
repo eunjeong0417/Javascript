b0a191136f094c8ee952fe145ce776f8e2fa6ffe
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-05-18 12:23:05 +0900

    Create object.js

---
+++ object.js
@@ -0,0 +1,56 @@
+
+const user = {
+    age:25,
+    name:"abc"
+};
+
+console.log(user);
+console.log(user.age);
+console.log(user.name);
+console.log(typeof user, user);
+
+user.age = 40;
+console.log(user.age);
+
+user.name = "cde"
+console.log(user.name);
+
+
+
+const user = {
+    age:25,
+    name:"abc",
+    fnc: function() {
+        return 1 + 5;
+    }
+};
+
+console.log(user.fnc());
+
+
+
+
+const a = {};
+a.name = 'abc'
+a.age = 20;
+a.fnc = function() {
+    return 1 + 3;
+};
+
+console.log(a.name);
+console.log(a.age);
+console.log(a.fnc());
+
+
+
+
+const user = {
+    age:25,
+    name:"abc",
+    fnc: function() {
+        return 1 + 5;
+    }
+};
+
+console.log(user.fnc());
+
