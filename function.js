ab7d446c405e0dd778e2861a0f900043ac6acf88
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-05-18 11:22:24 +0900

    Update function.js

--- function.js
+++ function.js
@@ -28,3 +28,61 @@ title.addEventListener("click", handleClick);
 function a(){
     document.getElementById('alert').style.display = 'block';
 }
+
+let f2 = function(age) {
+    return age + 2;
+}
+f2(3);
+
+
+let f2 = age => age + 2;
+console.log(f2(1));
+
+
+
+let func = (name) =>  console.log('Hi!' + name);
+
+func('eunjeong');
+
+
+
+
+let func = (name) => {
+    
+    console.log('Hi!' + name);
+}
+
+func('eunjeong');
+
+
+
+let func = () => {
+    
+    console.log('Hi!');
+}
+
+func();
+ 
+let f3 = function (a,b) {
+    return a + b;
+}
+f3(2,8);
+
+let f3 = (a,b) => a + b;
+f3(3,9);
+
+
+var a = {
+    b: function(){
+
+    }
+}
+
+var a = function(func) {
+    func();
+};
+var b = function() {
+    alert('Hi!')
+}
+a(b);
+
