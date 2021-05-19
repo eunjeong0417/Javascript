68d22f0e327bc7e71bf6fca51015cb7ce82f76f0
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-05-19 19:03:29 +0900

    Document Object Model
    document.querySelector('class')
    classList.add('class')
    classList.remove('class')
    classList.toggle('class')

---
+++ document.js
@@ -0,0 +1,35 @@
+/*const pElem = document.createElement('p')
+
+
+pElem.innerHTML = '<a href="#">Hello</a>'
+
+
+const charactersElem =  document.querySelector('.characters')
+
+charactersElem.appendChild(pElem)
+
+charactersElem.removeChild(document.querySelector('.b'))
+
+
+
+const ilbuni = document.querySelector('.ilbuni')
+
+ilbuni.classList.add('special')
+
+const ilbuni = document.querySelector('.ilbuni')
+
+ilbuni.classList.remove('ilbuni')*/
+
+const ilbuni = document.querySelector('.ilbuni')
+
+ilbuni.classList.toggle('special')
+
+ilbuni.classList.toggle('special')
+
+
+
+
+
+
+
+
